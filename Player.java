1ad4d989480eff09c028b3a9832a7009cc7b018b
Rohan <rohan.farmington@gmail.com>
2017-05-16 07:50:35 -0400

    Add files via upload

---
+++ Player.java
@@ -0,0 +1,80 @@
+package APAttack;
+
+import java.awt.Color;
+import java.awt.Point;
+
+public class Player extends Point{
+
+String name; 
+Color g = Constants.GREEN_COLOR;
+private boolean moveRight;
+private boolean moveLeft;
+private boolean moveUp;
+private int velY;
+private int velX;	
+	
+	public Player()	
+	{
+	this.setLocation(new Point(0, Constants.PLAYER_START_Y));
+	this.moveRight = false;
+	this.moveLeft = false;
+	this.moveUp = false;
+	
+	}
+
+	
+	public boolean isMovingRight() {
+		return moveRight;
+	}
+
+	public void setMoveRight() {
+		this.setLocation(this.getX() + this.velX, this.getY());
+	}
+
+	public boolean isMovingLeft() {
+		return moveLeft;
+	}
+
+	public void setMoveLeft() {
+		this.setLocation(this.getX() - this.velX, this.getY());
+	}
+
+	public boolean isMovingUp() {
+		return false;
+	}
+
+	public void setMoveUp(boolean moveUp) {
+		this.moveUp = moveUp;
+	}
+
+	public int getVelY() {
+		return velY;
+	}
+
+	public void setVelY(int velY) {
+		this.velY = velY;
+	}
+
+	public int getVelX() {
+		return velX;
+	}
+
+	public void setVelX(int velX) {
+		this.velX = velX;
+	}
+	
+	
+	public static void main(String[] args)
+	{
+	
+		Player p = new Player(); 
+		System.out.println(p.isMovingRight());
+		
+	}
+	
+	
+	
+	
+	
+	
+}
