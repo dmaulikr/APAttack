3c5ac3db9c0ea1b0988bac8d807ae872e8ef7e52
Rohan <rohan.farmington@gmail.com>
2017-05-25 08:07:42 -0400

    Add files via upload

---
+++ HealthBar.java
@@ -0,0 +1,42 @@
+package APAttack;
+
+import java.awt.Point;
+
+public class HealthBar extends Point{
+
+private float health;
+	
+	public HealthBar() {
+		this.health = Constants.HEALTH_MAX;
+	}
+	
+	public void addHealth() {
+		if (this.health < 100) {
+			this.health += 1;
+		}
+	}
+	
+	public void removeHealth() {
+		if (this.health > 0) {
+			this.health -= 1;
+		}
+	}
+
+	public float getHealth() {
+		return this.health;
+	}
+
+	public void setHealth(int health) {
+		this.health = health;
+	}
+
+	public double getX() {
+		return Constants.HEALTHBAR_X + Constants.HEALTHBAR_WIDTH - (int) (this.health / Constants.HEALTH_MAX * Constants.HEALTHBAR_WIDTH);
+	}
+
+	public float getHealthPercentage() {
+		return this.health / Constants.HEALTH_MAX;
+	}
+	
+	
+}
