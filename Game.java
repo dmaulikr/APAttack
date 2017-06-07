04e54b65e4f01f1a5059e95439f98e312c1a1014
Rohan <rohan.farmington@gmail.com>
2017-06-07 08:13:55 -0400

    Add files via upload

--- Game.java
+++ Game.java
@@ -1,25 +1,36 @@
+package APAttack;
+
+
+import javax.imageio.ImageIO;
 import javax.swing.*;
+
 import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
 import java.util.Random;
 
 public class Game extends JPanel {
 
     //changing these values will change the size of the game, while still remaining functional
     //within the size limit specified.
+	
     static int windowWidth = 1500;
     static int windowHeight = 1100;
-
+  //  BufferedImage textbook = ImageIO.read(new File("textbook.jpg"));
     int randNumb = 0;
     int circleWidth = 50;
     int circleHeight = 50;
     int circleYLocation = -circleHeight;
+ //   int textbookYLocation = -textbook.getHeight();
+    int playerYLocation;
     boolean numberCreated = false;
     static boolean gameRunning = false;
-
+   
     //generates a random Y value inside the window for the circle to spawn at
     public void generateRandomNumber() {
         Random rand = new Random();
-        randNumb = rand.nextInt(windowWidth - circleWidth);
+     //   randNumb = rand.nextInt(windowWidth - textbook.getWidth());
+        
         numberCreated = true;
     }
 
@@ -29,6 +40,7 @@ public class Game extends JPanel {
         g.fillRect(0, 0, windowWidth, windowHeight);
         g.setColor(Color.BLUE);
         g.fillOval(randNumb, circleYLocation, circleWidth, circleHeight);
+      //  g.drawImage(textbook, randNumb, textbookYLocation, null);
     }
 
     public void update() {
@@ -38,15 +50,14 @@ public class Game extends JPanel {
             generateRandomNumber();
         }
         //moves the circles y coordinate towards the bottom of the screen and stops once it hits the bottom
-        if (circleYLocation <= windowHeight) {
-            circleYLocation++;
-
+    //    if (textbookYLocation <= windowHeight) {
+     //
             //resets the x and y location to a new position
-        } else {
-            numberCreated = false;
-            circleYLocation = -circleHeight;
+      //  } else {
+      //      numberCreated = false;
+     //       textbookYLocation = -textbook.getHeight();
         }
-    }
+  //  }
 
     //sets the while loop to true to start the game
     public void start() {
